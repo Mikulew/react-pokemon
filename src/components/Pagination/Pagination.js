bc04de8fb4c21f2ff02d51abc4da7b1ce2661ecf
Mikulew <mikulew@gmail.com>
2019-10-06 16:39:55 +0200

    Pagination generates pages dynamically

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,25 +2,57 @@ import React, { Component } from 'react';
 
 class Pagination extends Component {
   state = {
-    pages: [1, 2, 3, 4, 5],
+    pages: [],
   };
+
+  componentDidMount() {
+    this.fetchPageNumbers();
+  }
+
+  range = (from, to, step = 1) => {
+    const range = [];
+
+    while (from <= to) {
+      range.push(from);
+      from += step;
+    }
+
+    return range;
+  };
+
+  fetchPageNumbers = () => {
+    const { limit, size } = this.props;
+    const totalPages = Math.ceil(size / limit);
+    const pages = this.range(1, totalPages);
+    this.setState({ pages });
+  };
+
+  componentDidUpdate(prevProps) {
+    const { limit } = this.props;
+
+    if (prevProps.limit !== limit) {
+      this.fetchPageNumbers();
+    }
+  }
 
   render() {
     const { setPage, activePage } = this.props;
     const { pages } = this.state;
 
     return (
-      <nav className="pagination-container">
-        <ul className="pagination">
-          {pages.map((page, index) => (
-            <li className={activePage === page ? 'page-item active' : 'page-item'} key={index}>
-              <button onClick={() => setPage(page)} className="page-link">
-                {page}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <div className="col-12">
+        <div className="pagination-container">
+          <ul className="pagination">
+            {pages.map((page, index) => (
+              <li className={activePage === page ? 'page-item active' : 'page-item'} key={index}>
+                <button onClick={() => setPage(page)} className="page-link">
+                  {page}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
