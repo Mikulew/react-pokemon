b6a7c44974d755ef0c7e1f7ef25c11d7c93cbe15
Mikulew <mikulew@gmail.com>
2019-10-11 21:17:48 +0200

    Page404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PokemonHeader from 'components/PokemonHeader/PokemonHeader';
 import { PokemonsContainer } from 'containers/PokemonsContainer';
 import PokemonDetails from 'components/PokemonDetails/PokemonDetails';
+import Page404 from 'components/Page404/Page404';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'duck/store';
@@ -14,6 +15,7 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={PokemonsContainer} />
           <Route exact path="/pokemon/:id" component={PokemonDetails} />
+          <Route component={Page404} />
         </Switch>
       </BrowserRouter>
     </Provider>
---
+++ src/components/Page404/Page404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page404 = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-12">
+        <h1 className="display-4 text-center mt-5">Pokémon not found</h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default Page404;
